1c057b48e698821bbc91767905770c541b181f2e
raspberian Robotinik <raspbot@raspbmc.(none)>
2013-12-10 11:44:04 -0200

    mudando estrutura

---
+++ tweetBot/raspTweet.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import sys
+import os
+import urllib2
+import time
+import random
+import json
+import ConfigParser
+
+from twython import Twython
+# arquivo com as configuralcoes de acesso do twitter
+from raspTweetConf import *
+
+api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
+config = ConfigParser.ConfigParser()
+config.readfp(open('defaults.cfg'))
+
+# variaveis uteis.
+hora = time.strftime("%d/%m/%Y - %H:%M:%S")
+
+#inicio MESMO
+if sys.argv[1] == 'listen':
+    if config.get('Enviroment','LAST_DM_ID'):
+        # pedir somente os itens maiores que o id existente...
+        current_val = config.get('Enviroment','LAST_DM_ID')
+        print 'Valor atual = '+current_val
+
+    else:
+        # variavel nao existe.
+        config.set('Enviroment','LAST_DM_ID',0)
+        config.write(open('defaults.cfg','w'))
+        #abort script.
+        exit()
+    #pegando pacote com as mensagens enviadas
+    pkgReturnDM = api.get_direct_messages(since_id=current_val)
+    for returnDM in pkgReturnDM:
+        print returnDM['created_at']
+        print returnDM['entities']['hashtags']
+elif sys.argv[1] == 'update':
+    api.update_status(status=sys.argv[2])
+elif sys.argv[1] == 'temp':
+    cmd = '/opt/vc/bin/vcgencmd measure_temp'
+    line = os.popen(cmd).readline().strip()
+    temp = line.split('=')[1].split("'")[0]
+    api.update_status(status='My current CPU temperature is '+temp+' C ['+hora+']')
+elif sys.argv[1] == 'addr':
+    line = urllib2.urlopen('http://checkip.dyndns.org').readline().strip()
+    addr = line.split("<body>")[1].split("</body>")[0]
+    addr = addr+' ['+hora+']'
+    api.send_direct_message(user='rom232',text=addr)
+elif sys.argv[1] == 'fortune':
+    cmd = 'fortune'
+    line = os.popen(cmd).read().strip()
+    if len(line) < 140:
+        api.update_status(status=line)
+elif sys.argv[1] == 'time':
+    api.update_status(status='hora: ['+hora+']')
+else:
+    print '--- nothing here ---'
